bdd94e377045e4a8d81ba5493dd76b2b1b0febd9
Hruthik SH <hruthiksh714@gmail.com>
2025-12-18 12:03:29 +0530

    Add files via upload

---
+++ financial_queries.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE finance_db;
+USE finance_db;
+CREATE TABLE financial_data (
+    Transaction_ID VARCHAR(10),
+    Date DATE,
+    Region VARCHAR(20),
+    Department VARCHAR(50),
+    Revenue DECIMAL(12,2),
+    Cost DECIMAL(12,2),
+    Profit DECIMAL(12,2)
+);
+SHOW TABLES;
+SELECT COUNT(*) FROM financial_data;
+SELECT SUM(Revenue) AS total_revenue FROM financial_data;
+SELECT SUM(Profit) AS total_profit FROM financial_data;
+SELECT 
+    SUM(Sales_Amount) AS total_sales,
+    SUM(Profit) AS total_profit
+FROM sales_data;
